0f1ad5117c0b750ef639906106b88ade7332304f
DavidPin-Git <davidpinheiro696@gmail.com>
2024-12-12 13:56:15 -0300

    Update Tkinker_GUI.py

--- Tkinker_GUI.py
+++ Tkinker_GUI.py
@@ -1,3 +1,4 @@
+import multiprocessing.process
 import customtkinter as ctk
 from customtkinter import filedialog
 from tkinterdnd2 import DND_FILES, TkinterDnD
@@ -7,62 +8,44 @@ from pydub import AudioSegment #importar pyaudio para funcionar
 from pydub.playback import play, _play_with_simpleaudio
 import threading
 import multiprocessing
-from datetime import datetime
+from datetime import datetime, timedelta
 from preprocess import preprocess
 from player import player
-#from visualizacao import bolas
 
 
 
 faixas = [0,0,0,0,0]
 faixasStr = ["vocals.wav","bass.wav","drums.wav","piano.wav","other.wav"]
-caminho = "All Guns Blazing.mp3"
+caminho = "Judas Priest - All Guns Blazing (Official Audio).mp3"
 tempoMusica = 0
-#thread_musica = None
 tempoInicioMusica = None
-play_obj = None
 paused = False
+musica = None
+procPlayer = None
+lenSong = 0
 
-'''
-def reproduzMusica(path, tempo, volume):
-    global play_obj, tempoMusica
-    musica = AudioSegment.from_mp3(path)
-    musica = musica - volume
-    #print(tempo)
-    play_obj = _play_with_simpleaudio(musica[(tempo*1000):])
-    #play(musica[(tempo * 1000):])
-'''
-def exportaMp3(musica):
-    musica.export("Musica_separada.mp3", format="mp3")
+def startTime(queue):
+    #print("Entrei datetimeNow\n")
+    queue.put(datetime.now())
 
 def reproduzMusica():
-    global caminho, tempoMusica, paused, play_obj, tempoInicioMusica, faixas
-    '''
-    thread_musica = threading.Thread(target=reproduzMusica, args=(caminho,tempoMusica,volume,), daemon=True)
-    thread_musica.start()
-    '''
-    nomeAudio = caminho.split("/")[-1].split(".")[0]
+    global paused, tempoInicioMusica, musica, procPlayer, tempoMusica
+    queue = multiprocessing.Queue()
 
-    musica = juntaAudio(faixas, nomeAudio)
     if not musica:
-        lblWarning.configure(text="Selecione uma faixa")
+        lblWarning.configure(text="Carregue uma musica")
     else:
         lblWarning.configure(text="")
-        volume = abs((sldSpeaker.get()/2)-50)
-        musica = musica - volume
 
-        thread_export = threading.Thread(target=exportaMp3, args=(musica,), daemon=True)
-        thread_export.start()
-        #musica.export("Musica_separada.mp3", format="mp3")
+        procPlayer = multiprocessing.Process(target=player, args=(tempoMusica,))
+        procTempo = multiprocessing.Process(target=startTime, args=(queue,))
+        procPlayer.daemon =True
+        procTempo.daemon = True
+        procPlayer.start()
+        procTempo.start()
 
-        #print("teste\n")
-        #play_obj = _play_with_simpleaudio(musica[(tempoMusica*1000):])
-        #play_obj = multiprocessing.Process(target=player)
-        player()
-
-
+        tempoInicioMusica = queue.get()
         paused = False
-        tempoInicioMusica = datetime.now()
 
         btnPausa.configure(state="normal")
         btnReproduz.configure(state="disabled")
@@ -82,60 +65,62 @@ def juntaAudio(faixas, arq):
             audio = audio.overlay(newAudio)
     return audio
 
-def reproduzir():
-    tatil = sldTatil.get()
-    speaker = sldSpeaker.get()
-    global faixas
-    faixas[0] = check1.get()
-    faixas[1] = check2.get()
-    faixas[2] = check3.get()
-    faixas[3] = check4.get()
-    faixas[4] = check5.get()
-
-    #print(f"Tatil: {tatil}%\tSpeaker: {speaker}%\nVocal: {vocal}\nGuitarra: {guitarra}\nBateria: {bateria}\nPiano: {piano}")
-    #threadBolas = threading.Thread(target=bolas, daemon=True)
-    #threadBolas.start()
-    reproduzMusica()
-    
-
-    #os.system("cd output")
-    #os.system("ffmpeg -i vocal.wav -vn -ar 44100 -ac 2 -b:a 192k vocal.mp3")
-
-
 
 def pausar():
-    global play_obj, tempoMusica, tempoInicioMusica, paused
+    global tempoMusica, tempoInicioMusica, paused, procPlayer
     paused = True
-    play_obj.stop()
+    procPlayer.terminate()
 
     deltaTempo = datetime.now() - tempoInicioMusica
-    tempoMusica += deltaTempo.total_seconds()
+    tempoMusica += deltaTempo.total_seconds() - 4
     #print(tempoMusica)    
 
     btnPausa.configure(state="disabled")
     btnReproduz.configure(state="normal")
 
 def check_playing(pausa):
-    global play_obj, tempoMusica, paused
-
-    if play_obj and not play_obj.is_playing() and not paused:
+    global tempoMusica, paused, procPlayer, lenSong, tempoInicioMusica
+    #print("check\n")
+    if tempoInicioMusica:
+        delta = timedelta(seconds=tempoMusica)
+        duracao = (datetime.now()-(tempoInicioMusica + delta)).total_seconds()
+    if procPlayer and duracao>=lenSong:
         btnPausa.configure(state="disabled")
         btnReproduz.configure(state="normal")
         #print("reset\n")
         tempoMusica = 0
-        play_obj = None
+        procPlayer.terminate()
+        procPlayer = None
+        paused = False
     
-    #pausa.after(500, lambda: check_playing(pausa))
+    pausa.after(500, lambda: check_playing(pausa))
 
 def processaAudio(path):
-    global caminho
+    global caminho, faixas, musica, lenSong, tempoMusica
+    tempoMusica = 0
     caminho = path
     separator = Separator('spleeter:5stems')
     separator.separate_to_file(path, 'output')
     nomeAudio = caminho.split("/")[-1].split(".")[0]
-    bateria = AudioSegment.from_wav("output/"+nomeAudio+"/"+"vocals.wav") ############################################
-    bateria.export("bateria.mp3",format="mp3")
-    preprocess("bateria.mp3")
+
+    faixas[0] = check1.get()
+    faixas[1] = check2.get()
+    faixas[2] = check3.get()
+    faixas[3] = check4.get()
+    faixas[4] = check5.get()
+
+    musica = juntaAudio(faixas, nomeAudio)
+    if not musica:
+        musica = AudioSegment.from_mp3(path)
+    else:
+        lblWarning.configure(text="")
+        volume = abs((sldSpeaker.get()/2)-50)
+        musica = musica - volume
+    lenSong =len(musica)/1000
+    print("Tamanho da musica: "+str(lenSong))
+    musica.export("prepMusica.mp3",format="mp3")
+    preprocess("prepMusica.mp3")
+
     btnReproduz.configure(state="normal")
     print("Separação concluída. Arquivos salvos na pasta 'output'.")
 
@@ -181,7 +166,7 @@ if __name__ == '__main__':
 
     #Componentes da janela
     audio = ctk.CTkTextbox(janela, activate_scrollbars=False)
-    audio.place(relx=0.15, rely =0.05, relheight= 0.2)
+    audio.place(relx=0.1, rely =0.55, relheight= 0.2)
     audio.insert("1.0","Arraste um audio")
     audio.configure(state="disabled")
 
@@ -189,48 +174,48 @@ if __name__ == '__main__':
     audio.dnd_bind("<<Drop>>", arrasta)
 
     btnCarrega = ctk.CTkButton(janela,text="Carregar",command=carregaAudio)
-    btnCarrega.place(relx = 0.6, rely =0.05)
+    btnCarrega.place(relx = 0.6, rely =0.6)
 
     lblFaixa = ctk.CTkLabel(janela,text="Faixas")
-    lblFaixa.place(relx=0.1, rely=0.25)
+    lblFaixa.place(relx=0.1, rely=0.05)
 
     check1 = ctk.CTkCheckBox(janela, text="Vocal")
-    check1.place(relx=0.1, rely=0.35)
+    check1.place(relx=0.1, rely=0.15)
 
     check2 = ctk.CTkCheckBox(janela, text="Baixo")
-    check2.place(relx=0.4, rely=0.35)
+    check2.place(relx=0.4, rely=0.15)
 
     check3 = ctk.CTkCheckBox(janela, text="Bateria")
-    check3.place(relx=0.1, rely=0.45)
+    check3.place(relx=0.1, rely=0.25)
 
     check4 = ctk.CTkCheckBox(janela, text="Piano")
-    check4.place(relx=0.4, rely=0.45)
+    check4.place(relx=0.4, rely=0.25)
 
     check5 = ctk.CTkCheckBox(janela, text="Outros")
-    check5.place(relx=0.7, rely=0.35)
+    check5.place(relx=0.7, rely=0.15)
 
     lblTatil = ctk.CTkLabel(janela,text="Tatil")
-    lblTatil.place(relx=0.1, rely=0.55)
+    lblTatil.place(relx=0.1, rely=0.35)
 
     lblSpeaker = ctk.CTkLabel(janela,text="Speaker")
-    lblSpeaker.place(relx=0.5, rely=0.55)
+    lblSpeaker.place(relx=0.5, rely=0.35)
 
     sldTatil = ctk.CTkSlider(janela, from_=0, to=100, command=sliderTatil,number_of_steps=200)
-    sldTatil.place(relx=0.1, rely=0.65)
+    sldTatil.place(relx=0.1, rely=0.45)
 
     lblVolTatil = ctk.CTkLabel(janela, text="50%", height=8)
-    lblVolTatil.place(relx=0.27, rely=0.71)
+    lblVolTatil.place(relx=0.27, rely=0.39)
 
     lblVolSpeaker = ctk.CTkLabel(janela, text="50%", height=8)
-    lblVolSpeaker.place(relx=0.67, rely=0.71)
+    lblVolSpeaker.place(relx=0.67, rely=0.39)
 
     sldSpeaker = ctk.CTkSlider(janela, from_=0, to=100, command=sliderSpk,number_of_steps=200)
-    sldSpeaker.place(relx=0.5, rely=0.65)
+    sldSpeaker.place(relx=0.5, rely=0.45)
 
     lblWarning = ctk.CTkLabel(janela, text="", text_color="red")
     lblWarning.place(relx=0.375, rely=0.9)
 
-    btnReproduz = ctk.CTkButton(janela,text="Reproduz", command=reproduzir)
+    btnReproduz = ctk.CTkButton(janela,text="Reproduz", command=reproduzMusica)
     btnReproduz.place(relx=0.1, rely=0.8)
 
     btnPausa = ctk.CTkButton(janela,text="Pausar", command=pausar)
